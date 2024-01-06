471804a6c8d7b3541812d5c58a7ec887c608244f
Rodrigo <rock.gule@gmail.com>
2024-01-06 03:03:31 -0600

    UPDATE 2.0

---
+++ Ejercicios/Contar-Strings/cmake-build-debug/CMakeFiles/Contar_Strings.dir/DependInfo.cmake
@@ -0,0 +1,19 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "C:/Users/rlope/OneDrive/Documentos/GitHub/Ejercicios-Computacionales/Ejercicios/Contar-Strings/main.c" "CMakeFiles/Contar_Strings.dir/main.c.obj" "gcc" "CMakeFiles/Contar_Strings.dir/main.c.obj.d"
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ Ejercicios/Contar-Strings/cmake-build-debug/CMakeFiles/Contar_Strings.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Contar_Strings.dir/main.c.obj"
+  "CMakeFiles/Contar_Strings.dir/main.c.obj.d"
+  "Contar_Strings.exe"
+  "Contar_Strings.exe.manifest"
+  "Contar_Strings.pdb"
+  "libContar_Strings.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/Contar_Strings.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
